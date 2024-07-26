3ea00ffb8d23c3cddd5abcd098e30c2fbd2e277b
kailaskg <kailaskg1447@gmail.com>
2024-07-26 15:56:42 +0530

    first commit, files and folders added to first commit

---
+++ JS/arrays.js
@@ -0,0 +1,20 @@
+let arr1=["kailas",50, "swayam", 3453]
+console.log(arr1.length);
+
+let arr2= new Array();
+console.log(arr2.length);
+arr2.push("kailas")
+console.log(arr2);
+
+
+let arr3= new Array(5)
+console.log(arr3.length);
+
+arr3.push(1000)
+arr3.push(2000)
+arr3[1]=3000
+console.log(arr3.unshift(4000));
+console.log(arr3);
+
+let arr4= new Array(5,7)
+console.log(arr4.length);
